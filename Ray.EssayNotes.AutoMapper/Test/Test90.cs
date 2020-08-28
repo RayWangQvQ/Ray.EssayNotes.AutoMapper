dfd4ac7a3209dcf581ca4de10fd69f28dbdfe46d
RayWang <2271272653@qq.com>
2020-08-28 14:11:47 +0800

    update

--- Ray.EssayNotes.AutoMapper/Test/Test90.cs
+++ Ray.EssayNotes.AutoMapper/Test/Test90.cs
@@ -9,36 +9,28 @@ using Ray.Infrastructure.EssayTest;
 
 namespace Ray.EssayNotes.AutoMapper.Test
 {
-    [Description("映射时的覆盖")]
-    public class Test30 : ITest
+    [Description("自定义映射规则")]
+    public class Test90 : ITest
     {
         public void Run()
         {
             var config = new MapperConfiguration(cfg => cfg.AddMaps(typeof(Test11).Assembly));
             IMapper mapper = config.CreateMapper();
 
-            UserEntity entity = new UserEntity("测试覆盖", "123", 25);
+            UserEntity userEntity = new UserEntity("测试", "123", 25);
 
-            UserDto dto = new UserDto
-            {
-                Id = 1,//entity没有
-                Name = "newName"
-            };
+            UserDto dto = mapper.Map<UserEntity, UserDto>(userEntity);
 
-            mapper.Map(dto, entity);
-
-            Console.WriteLine(entity.AsFormatJsonStr());
-
-            /*
-             * 对于已存在的Destination对象，映射时是全部覆盖，不会保留原Destination对象的值
-             */
+            Console.WriteLine(dto.AsFormatJsonStr());
         }
 
         public class UserProfile : Profile
         {
             public UserProfile()
             {
-                CreateMap<UserDto, UserEntity>();
+                CreateMap<UserEntity, UserDto>()
+                    .ForMember("Name", opt => opt.MapFrom(src => src.Pwd))
+                    .ForMember(dest => dest.Pwd, opt => opt.MapFrom(src => src.Name));
             }
         }
     }
