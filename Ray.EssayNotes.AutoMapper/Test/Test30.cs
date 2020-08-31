59fab99f0d4b5b3297dc66d5b64581ed14026041
RayWang <2271272653@qq.com>
2020-08-31 15:33:52 +0800

    Update

--- Ray.EssayNotes.AutoMapper/Test/Test30.cs
+++ Ray.EssayNotes.AutoMapper/Test/Test30.cs
@@ -18,6 +18,7 @@ namespace Ray.EssayNotes.AutoMapper.Test
             IMapper mapper = config.CreateMapper();
 
             UserEntity entity = new UserEntity("测试覆盖", "123", 25);
+            PrintEntity(entity);
 
             UserDto dto = new UserDto
             {
@@ -26,11 +27,20 @@ namespace Ray.EssayNotes.AutoMapper.Test
             };
 
             mapper.Map(dto, entity);
+            PrintEntity(entity);
 
-            Console.WriteLine(entity.AsFormatJsonStr());
+
+            void PrintEntity(UserEntity entity)
+            {
+                Console.WriteLine($"HashCode：{entity.GetHashCode()}");
+                Console.WriteLine(entity.AsFormatJsonStr());
+            }
 
             /*
-             * 对于已存在的Destination对象，映射时是全部覆盖，不会保留原Destination对象的值
+             * 对于已存在的Destination对象，映射时是全部覆盖，
+             * Source为null则覆盖为Null
+             * 不会保留原Destination对象的值
+             * 单Destination的引用地址不会变（即非深拷贝）
              */
         }
 
